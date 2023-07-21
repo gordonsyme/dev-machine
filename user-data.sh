752141f2ea77352341b69808ac79375ca5df8734
Gordon Syme <gordonsyme@gmail.com>
2023-07-21 15:48:57 +0100

    Install Temurin Java, replace pyyaml with yq

--- user-data.sh
+++ user-data.sh
@@ -38,6 +38,12 @@ apt-get install --quiet --yes \
   software-properties-common
 apt-get --yes upgrade
 
+# Install java
+curl -fsSL https://packages.adoptium.net/artifactory/api/gpg/key/public | apt-key add -
+echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
+apt-get update
+apt-get install temurin-11-jdk
+
 # Install docker
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -68,7 +74,9 @@ curl --silent --output /usr/local/bin/lein "https://raw.githubusercontent.com/te
 chmod a+x /usr/local/bin/lein
 
 # Install gordonsyme/docker-tools
-pip2 install pyyaml
+curl --location --output /usr/bin/yq "https://github.com/mikefarah/yq/releases/v4.34.2/download/yq_linux_arm64"
+chmod a+x /usr/bin/yq
+sudo --set-home --user=ubuntu mkdir /home/ubuntu/bin
 sudo --set-home --user=ubuntu curl \
   --location \
   --output /home/ubuntu/.bash_completion \
@@ -83,6 +91,9 @@ sudo --set-home --user=ubuntu curl \
   --location \
   --output /home/ubuntu/bin/dc \
   "https://raw.githubusercontent.com/gordonsyme/docker-tools/master/bin/dc"
+
+sudo --set-home --user=ubuntu chmod u+x /home/ubuntu/bin/d
+sudo --set-home --user=ubuntu chmod u+x /home/ubuntu/bin/dc
 
 # Set up the ubuntu user's bashrc
 BASHRC=/home/ubuntu/.bashrc
